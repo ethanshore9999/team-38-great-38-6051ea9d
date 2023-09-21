dbcfdab79959c6b00cd92e93a80c6c7d4c8ea089
ushacn <usha.cn@gmail.com>
2023-09-21 14:59:21 +0000

    starting character

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -3,3 +3,9 @@ class Character:
 
     def __init__(self, character_name):
         self.name = character_name
+
+    def getName(self):
+        return self.name
+    
+    def enterMap(self, map):
+        return 
--- tests/test_character.py
+++ tests/test_character.py
@@ -1,8 +1,20 @@
 from unittest import TestCase
 from levelup.character import Character
-
+from levelup.map import Map
 class TestCharacterInitWithName(TestCase):
     def test_init(self):
         ARBITRARY_NAME = "MyName"
         testobj = Character(ARBITRARY_NAME)
         self.assertEqual(ARBITRARY_NAME, testobj.name)
+    
+    def test_get_name(self):
+        ARBITRARY_NAME = "Bob"
+        testobj = Character(ARBITRARY_NAME);
+        result = testobj.getName();
+        self.assertEqual(ARBITRARY_NAME, result)
+
+    def test_enter_map(self):
+        map = Map();
+        ARBITRARY_NAME = "Bob";
+        testobj = Character(ARBITRARY_NAME);
+        testobj.enterMap(map)
