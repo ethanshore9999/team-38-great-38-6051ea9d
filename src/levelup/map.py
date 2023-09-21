5b87c7316b82a013fa8f767fa695333f2c934543
simmonsdt <obfuscatemail@gmail.com>
2023-09-21 20:04:25 +0000

    Created Positions Array in Map

--- src/levelup/map.py
+++ src/levelup/map.py
@@ -1,23 +1,30 @@
+from levelup.position import Position
 class Map:
     numPositions = 100;
-    arrPositions = [0,0];
+    map_data = [0,0];
     #calculatePosition = [0,0];
-    
        
     def __init__(self):
-        self.map_data = []
+        # Creating a 2D array of CustomObject instances
+        rows = 10
+        cols = 10
 
+        self.map_data = [
+            [Position((i,j)) 
+            for j in range(cols)] 
+                for i in range(rows)
+        ]
+  
     def __repr__(self):
         return "Map()"
 
     def getTotalPositions(self):
         return self.numPositions;
-
+# TODO
     def getPositions(self):
-        
         return self.getPositions;
     
-    def calculatePosition(self):
+    def calculatePosition(self, ):
         return self.calculatePosition;
 
     def isPositionValid(self):
--- tests/test_map.py
+++ tests/test_map.py
@@ -3,6 +3,10 @@ from levelup.map import Map
 from levelup.position import Position
 
 class TestMap(TestCase):
+    def test_init(self):
+        testObj = Map()
+        assert testObj.map_data[0][0].position == (0,0)
+        assert testObj.map_data[9][9].position == (9,9)
 
     def test_get_total_positions(self):
         testObj = Map()
