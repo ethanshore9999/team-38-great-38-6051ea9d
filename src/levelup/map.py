28ae7d08aa87809f203ea91fe2581eb6c301e83e
simmonsdt <obfuscatemail@gmail.com>
2023-09-21 20:57:23 +0000

    Updated calculatePositions in Map

--- src/levelup/map.py
+++ src/levelup/map.py
@@ -1,4 +1,5 @@
 from levelup.position import Position
+from levelup.controller import Direction
 class Map:
     numPositions = 100;
     map_data = [0,0];
@@ -25,7 +26,22 @@ class Map:
         return self.map_data;
     
     def calculatePosition(self, startingPosition, direction):
-        return self.calculatePosition;
+        new_x = startingPosition.position[0]
+        new_y = startingPosition.position[1]
+        if direction == Direction.NORTH:
+            new_y += 1
+        elif direction == Direction.SOUTH:
+            new_y -= 1
+        elif direction == Direction.WEST:
+            new_x -= 1
+        elif direction == Direction.EAST:
+            new_x += 1  
+        new_position = Positions(new_x, new_y)      
+        result = isPositionValid(new_position)
+        if result == True:
+            return new_position
+        else:
+            return startingPosition
 
     def isPositionValid(self, positionCoordinates):
         x = positionCoordinates.position[0]
